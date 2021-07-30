2cb35a83ec7a34a64d73b6b866ba798087f31608
nick_richardson <nick.richardson@rezonence.com>
2021-07-30 11:28:39 +0100

    Test booking filter validation

--- validator/validator.spec.ts
+++ validator/validator.spec.ts
@@ -1,4 +1,5 @@
 import {Validator} from "./validator";
+import {BookingStatus} from "../booking-data-dao/booking.status";
 
 describe("Validator", () => {
 
@@ -13,5 +14,20 @@ describe("Validator", () => {
     test("Marks input invalid if not string or undefined", () => {
         expect(Validator.isStringOrUndefined(12345)).toBe(false);
         expect(Validator.isStringOrUndefined({foo: "bar"})).toBe(false);
+    })
+
+    test("Validates booking filter requests", () => {
+        const request = {date: new Date(), status: BookingStatus.Completed, venue: "bar"};
+        expect(Validator.isValidBookingFilterRequest(request)).toBe(true);
+    })
+
+    test("Marks filter requests invalid if date invalid", () => {
+        const badRequest = {date: {foo: "bar"}, status: BookingStatus.Pending, venue: "foo"};
+        expect(Validator.isValidBookingFilterRequest(badRequest)).toBe(false);
+    })
+
+    test("Marks filter requests invalid if status invalid", () => {
+        const badRequest = {date: new Date(), status: "baz", venue: "baz"};
+        expect(Validator.isValidBookingFilterRequest(badRequest)).toBe(false);
     })
 })
